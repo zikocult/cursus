e74e5cff52eeedb63c3bee7a1ab38a6fc2e87aac
zikocult <gbarulls@gmail.com>
2023-01-11 13:02:08 +0100

    memset ready

--- 01_ring/main.c
+++ 01_ring/main.c
@@ -11,8 +11,9 @@ int main(int argc, char **argv)
 	  char str[] = "GeeksForGeeks is for programming geeks.";
 	  char str2[] = "GeeksForGeeks is for programming geeks.";
 	  printf("La frase original = %s\n", str);
-    printf("El resultado de mi funcion = %s", ft_memset(str, 'n', sizeof(str)));
+    printf("El resultado de mi funcion = %s", ft_memset(str + 4, 'n', 10));
 	  printf("\nEl resultado del original  = %s \n", memset(str2 + 10, 'n', 6) );
+    printf("Str = %s\nStr2 = %s", str, str2);
   }
 	return (0);
 }
