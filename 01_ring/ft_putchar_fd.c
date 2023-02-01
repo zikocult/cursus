e3d7ed6fea96e6c5cb2477c7df0e8826508fc0c3
Guillem <gbarulls@gmail.com>
2023-02-01 21:34:17 +0100

    Por hoy basta

--- 01_ring/ft_putchar_fd.c
+++ 01_ring/ft_putchar_fd.c
@@ -1,10 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbarulls <gbarulls@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 21:03:58 by gbarulls          #+#    #+#             */
+/*   Updated: 2023/02/01 21:04:01 by gbarulls         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+#include <fcntl.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
 
-
-
-
+// int main (void)
+// {
+// 	int fd;
+//
+// 	fd = open("/Users/gbarulls/tests/cursus/01_ring/prueba.txt", O_WRONLY);
+// 	if (fd == -1)
+// 		printf("Error al abrir el archivo\n");
+// 	ft_putchar_fd('a', fd);
+// 	close(fd);
+// 	return (0);
+// }
