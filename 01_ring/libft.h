3350f814814f0a61439d9088ff40381f33ed96ea
Guillem <gbarulls@gmail.com>
2023-02-01 20:39:33 +0100

    Seguimos adelante

--- 01_ring/libft.h
+++ 01_ring/libft.h
@@ -17,6 +17,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <unistd.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -48,5 +49,6 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	**ft_split(char const *s, char c);
+void	ft_putchar_fd(char c, int fd);
 
 #endif
