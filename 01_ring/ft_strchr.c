c1bef2857aa659f82514a0140e452b0a225adbe1
zikocult <gbarulls@gmail.com>
2023-02-02 22:37:47 +0100

    Last correction

--- 01_ring/ft_strchr.c
+++ 01_ring/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: gbarulls <gbarulls@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 00:12:08 by gbarulls          #+#    #+#             */
-/*   Updated: 2023/02/02 22:33:27 by gbarulls         ###   ########.fr       */
+/*   Updated: 2023/02/02 22:37:07 by gbarulls         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,3 @@ char	*ft_strchr(const char *str, int c)
 		return (ptr);
 	return (NULL);
 }
-
