78283019b5a3d8d0dfc56b700950bb106080ab1f
Guillem <gbarulls@gmail.com>
2023-02-03 18:09:59 +0100

    split begin

--- 01_ring/ft_split.c
+++ 01_ring/ft_split.c
@@ -6,17 +6,57 @@
 /*   By: gbarulls <gbarulls@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 23:59:19 by gbarulls          #+#    #+#             */
-/*   Updated: 2023/02/02 22:42:48 by gbarulls         ###   ########.fr       */
+/*   Updated: 2023/02/03 17:43:16 by gbarulls         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+- S'hauran de contar les paraules fins al char c, contant aixi el tamany del primer malloc
+- El apuntador, de apuntador es el que donara la seguent dimensio, es a dir, haure de fer el primer malloc sobre el numero de paraules
+- 
+*/
 
 #include "libft.h"
+#include <stdio.h>
 
-char **ft_split(char const *s, char c)
+static int ft_count_words(char const *s, char c)
 {
-	/*
-	s'hauran de contar les paraules fins al char c, contant aixi el tamany
-	un cop tinguem el tamany, s'haura de guardar en una dimensio de un malloc
-	el apuntador, de apuntador es el que donara la seguent dimensio
-	*/
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	int		i;
+	int		start;
+
+	i = 0;
+	start = 0;
+	split = (char **)malloc(sizeof(char) * ft_count_words(s, c) + 1);
+	if (!split)
+		return (NULL);
+	while (s[i])
+	{
+		
+	}
+	free (split);
+	return (split);
+}
+
+int main (void){
+	char prueba1[100] = "Vamos\ta\tprobar\tque\ttal\tfunciona\testo"; 
+	printf("%s\n", prueba1);
+	printf("%i\n", ft_count_words(prueba1, '\t'));
+	
+	return (0);
 }
